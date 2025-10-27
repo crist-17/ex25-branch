3f5d4e3806929a72f38d2cb524d9721f0a90c72a
crist-17 <pci1234good@gmail.com>
2025-10-27 16:37:52 +0900

    add 값검증

--- ex25-branch/src/main/java/com/pci/ex25branch/domain/Student.java
+++ ex25-branch/src/main/java/com/pci/ex25branch/domain/Student.java
@@ -1,15 +1,24 @@
 package com.pci.ex25branch.domain;
 
-import java.time.LocalDateTime;
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class Student {
 
     private Long id;
+
+    @NotBlank(message = "이름은 필수항목")
     private String name;
+
+    @Email(message = "이메일형식@ 입력필수")
     private String email;
+
+    @Min(value = 1, message = "나이는 1세이상")
     private Integer age;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
