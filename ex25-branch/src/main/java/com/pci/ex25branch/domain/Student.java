f2701761f6611d3245d14cc52dda8f263d32c2dd
crist-17 <pci1234good@gmail.com>
2025-10-28 16:43:06 +0900

    Squashed commit of the following:
    commit c7250cbab4aa00e3c66b8957935e1d20a996a0df
    Author: crist-17 <pci1234good@gmail.com>
    Date:   Tue Oct 28 14:52:24 2025 +0900
    
        1028add
    
    commit d70088ccaa7d4a0a7afcb3b941df54f0e32ac37f
    Merge: 8bf6dbb 08665f9
    Author: crist-17 <pci1234good@gmail.com>
    Date:   Tue Oct 28 09:36:21 2025 +0900
    
        Merge pull request #1 from crist-17/Develop
    
        Develop
    
    commit 08665f95fd290c7eef2eb329ee647f2eeb4a0125
    Author: crist-17 <pci1234good@gmail.com>
    Date:   Tue Oct 28 09:17:58 2025 +0900
    
        Update application.properties
    
    commit dce632ba31ea28d1a272ccf7c8dd31f92a360a91
    Author: crist-17 <pci1234good@gmail.com>
    Date:   Mon Oct 27 15:23:39 2025 +0900
    
        add

--- ex25-branch/src/main/java/com/pci/ex25branch/domain/Student.java
+++ ex25-branch/src/main/java/com/pci/ex25branch/domain/Student.java
@@ -8,8 +8,14 @@ import lombok.Data;
 public class Student {
 
     private Long id;
+
+    @NotBlank(message = "이름은 필수항목")
     private String name;
+
+    @Email(message = "이메일형식@ 입력필수")
     private String email;
+
+    @Min(value = 1, message = "나이는 1세이상")
     private Integer age;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
