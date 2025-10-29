e1abbdbd3c1f401f772711b1dd7d2173fcda2f97
crist-17 <pci1234good@gmail.com>
2025-10-30 00:35:51 +0900

    Squashed commit of the following:
    commit 63c0ef922529385823ad666725ce65b364210022
    Author: crist-17 <pci1234good@gmail.com>
    Date:   Wed Oct 29 12:30:47 2025 +0900
    
        add>1028mappersql

--- ex25-branch/src/main/java/com/pci/ex25branch/mapper/StudentMapper.java
+++ ex25-branch/src/main/java/com/pci/ex25branch/mapper/StudentMapper.java
@@ -1,33 +1,33 @@
 package com.pci.ex25branch.mapper;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-
 import com.pci.ex25branch.domain.Student;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface StudentMapper {
 
+    @Select("SELECT * from student ORDER BY id DESC ")
     List<Student> findAll();
 
+
+	@Select("SELECT * FROM student WHERE id = #{id}")
     Student findById(Long id);
-//	@Select("SELECT * FROM student WHERE id = #{id}")
-//	Student findById(Long id);
 
-    void insert(Student student);
-//	@Insert("INSERT INTO student(NAME, email, age) \r\n"
-//			+ "  	VALUES (#{name}, #{email}, #{age})")
-//	@Options(useGeneratedKeys = true, keyProperty = "id")
-//	void insert(Student student);
 
-    void update(Student student);
-//	@Update("UPDATE student\r\n"
-//			+ "	SET NAME = #{name}, email = #{email}, age = #{age}\r\n"
-//			+ "	WHERE id = #{id}")
-//	void update(Student student);
+	@Insert("INSERT INTO student(NAME, email, age) \r\n"
+			+ "  	VALUES (#{name}, #{email}, #{age})")
+	@Options(useGeneratedKeys = true, keyProperty = "id")
+	void insert(Student student);
 
-    void delete(Long id);
-//	@Delete("DELETE FROM student WHERE id = #{id}")
-//	void delete(Long id);
+
+	@Update("UPDATE student\r\n"
+			+ "	SET NAME = #{name}, email = #{email}, age = #{age}\r\n"
+			+ "	WHERE id = #{id}")
+	void update(Student student);
+
+
+	@Delete("DELETE FROM student WHERE id = #{id}")
+	void delete(Long id);
 }
