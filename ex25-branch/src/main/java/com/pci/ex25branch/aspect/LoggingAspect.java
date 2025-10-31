880ccfc511023f9b104f8ad3f0b633d3c78f0aa0
crist-17 <pci1234good@gmail.com>
2025-10-31 17:33:02 +0900

    add branch > aop

---
+++ ex25-branch/src/main/java/com/pci/ex25branch/aspect/LoggingAspect.java
@@ -0,0 +1,25 @@
+package com.pci.ex25branch.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* com.pci.ex25branch.service.*.*(..))")
+    public void logBefore(JoinPoint jp){
+        log.info("---메서드 실행전: {} MMMMM", jp.getSignature());
+    }
+
+    @AfterReturning (pointcut = "execution(* com.pci.ex25branch.service.*.*(..))", returning = "result")
+    public void logAfter(JoinPoint jp,Object result){
+        log.info("---- 메서드 실행성공: {} ㅈㅈㅈㅈ \n ㅈㅈㅈㅈㅈ {} ㅈㅈㅈㅈㅈ", jp.getSignature(), result);
+    }
+
+}
---
+++ ex25-branch/src/main/java/com/pci/ex25branch/aspect/PerformanceAspect.java
@@ -0,0 +1,21 @@
+package com.pci.ex25branch.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class PerformanceAspect {
+    @Around("execution(* com.pci.ex25branch.service.*.*(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = pjp.proceed();
+        long end = System.currentTimeMillis();
+        log.info("<<<<<abcde>>>>> 메서드 실행시간: {} >>>>>>> \n <<<<df3322xxx>>>>>> {} >>>>", pjp.getSignature(), (end - start));
+    return result;
+    }
+}
